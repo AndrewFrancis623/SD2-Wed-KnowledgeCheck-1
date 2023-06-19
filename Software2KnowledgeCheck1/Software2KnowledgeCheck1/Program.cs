75311b31d0b91fa5bcfb192f0a41d6d72cba2927
Andrew Francis <afrancis623@icloud.com>
2023-06-18 23:14:39 -0400

    Updated

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+
+using Software2KnowledgeCheck1;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        var constructionlogic = new ConstructionLogic();
+        var apartment = new Apartment();
+        var highrise = new HighRise();
+        var building = new Building();
+
+        var buildings = new List<Building>() { highrise, building};
+
+        constructionlogic.CreateUnit(apartment, buildings);
+
+
+    }
+
+}
